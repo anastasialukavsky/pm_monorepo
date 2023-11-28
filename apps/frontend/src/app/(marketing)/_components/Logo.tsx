e813103ef32ce0a452f8a9b7a7078dc699616249
ruralFluffyDill <stacylukavsky@gmail.com>
2023-11-27 21:57:10 -0500

    add subset to render logo

--- apps/frontend/src/app/(marketing)/_components/Logo.tsx
+++ apps/frontend/src/app/(marketing)/_components/Logo.tsx
@@ -4,11 +4,12 @@ import { Archivo_Black } from 'next/font/google';
 const archivo = Archivo_Black({
   style: 'normal',
   weight: '400',
+  subsets: ['latin'],
 });
 export default function Logo() {
   return (
     <div className={`${archivo.className} text-3xl text-slate-700`}>
-      TaskTide
+      TT
     </div>
   );
 }
