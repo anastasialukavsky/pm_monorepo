08826e7bf21e260eecbf16811e1b5f304d461717
ruralFluffyDill <stacylukavsky@gmail.com>
2023-11-27 21:57:20 -0500

    render logo component

--- apps/frontend/src/app/(marketing)/_components/Navbar.tsx
+++ apps/frontend/src/app/(marketing)/_components/Navbar.tsx
@@ -4,7 +4,7 @@ import Logo from './Logo';
 export default function Navbar() {
   return (
     <div className="h-[48px] w-[100vw] fixed top-0 border border-b-slate-700 p-3">
-      {/* <Logo /> */}
+      <Logo />
     </div>
   );
 }
