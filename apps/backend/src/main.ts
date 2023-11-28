95f43aba8a1d3a2d00b534c9297f980c600e83b6
ruralFluffyDill <stacylukavsky@gmail.com>
2023-11-27 21:55:44 -0500

    change origin to 3000

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
   if (!SESSION_SECRET)
     throw new NotFoundException('Cannot extract session secret');
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: 'http://localhost:3000',
     credentials: true, // Allow credentials (cookies, headers, etc.)
   });
   app.use(
