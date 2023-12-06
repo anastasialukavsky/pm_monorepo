eef212ce262b4b9399f5420bb71d47c531ee4d3c
ruralFluffyDill <stacylukavsky@gmail.com>
2023-12-05 19:17:12 -0500

    add redis store config

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -10,11 +10,23 @@ import { BookmarkModule } from './bookmark/bookmark.module';
 import { ProjectModule } from './project/project.module';
 import { TaskModule } from './task/task.module';
 import { AuthMiddleware } from './auth/middleware';
+// import { CacheModule } from '@nestjs/common';
+import * as redisStore from 'cache-manager-redis-store';
+// import { Module } from '@nestjs/common';
+import { CacheModule } from '@nestjs/cache-manager';
+// import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
     }),
     AuthModule,
     UserModule,
@@ -23,6 +35,7 @@ import { AuthMiddleware } from './auth/middleware';
     ProjectModule,
     TaskModule,
   ],
+  providers: [AppService],
   // controllers: [ProjectController],
 })
 export class AppModule implements NestModule {
