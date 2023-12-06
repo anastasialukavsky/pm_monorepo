757fd53bc27f1dcf15390202eadca1410d34b3f9
ruralFluffyDill <stacylukavsky@gmail.com>
2023-12-05 19:18:11 -0500

    add loger to authMiddleware

--- apps/backend/src/auth/middleware/auth.middleware.ts
+++ apps/backend/src/auth/middleware/auth.middleware.ts
@@ -1,9 +1,31 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
+
+// @Injectable()
+// export class AuthMiddleware implements NestMiddleware {
+//   use(req: Request, res: Response, next: NextFunction) {
+
+//     const authToken = req.headers['authorization'];
+
+//     if (!authToken) {
+//       return res
+//         .status(401)
+//         .json({ message: 'Unauthorized - Missing Authorization Header' });
+//     }
+
+//     next();
+//   }
+// }
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
+  private readonly logger = new Logger(AuthMiddleware.name);
+
+  // constructor(private readonly requestService: RequestService) {}
+
   use(req: Request, res: Response, next: NextFunction) {
+    this.logger.log(AuthMiddleware.name);
+
     const authToken = req.headers['authorization'];
 
     if (!authToken) {
