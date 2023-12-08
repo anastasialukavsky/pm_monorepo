9b9d468413b371070981f0a9cdb9ba2dbb41d365
ruralFluffyDill <stacylukavsky@gmail.com>
2023-12-07 20:36:15 -0500

    add token validity checking auth middleware

--- apps/backend/src/auth/middleware/auth.middleware.ts
+++ apps/backend/src/auth/middleware/auth.middleware.ts
@@ -1,5 +1,35 @@
 import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
+import { AuthService } from '../auth.service';
+
+@Injectable()
+export class AuthMiddleware implements NestMiddleware {
+  private readonly logger = new Logger(AuthMiddleware.name);
+
+  // constructor(private readonly requestService: RequestService) {}
+
+  constructor(private readonly authService: AuthService) {}
+
+  async use(req: Request, res: Response, next: NextFunction) {
+    this.logger.log(AuthMiddleware.name);
+
+    const authToken = req.headers['authorization'];
+
+    if (!authToken) {
+      return res
+        .status(401)
+        .json({ message: 'Unauthorized - Missing Authorization Header' });
+    }
+
+    try {
+      // const decodedAT =
+    } catch (err) {
+      throw err;
+    }
+
+    next();
+  }
+}
 
 // @Injectable()
 // export class AuthMiddleware implements NestMiddleware {
@@ -16,24 +46,3 @@ import { NextFunction, Request, Response } from 'express';
 //     next();
 //   }
 // }
-
-@Injectable()
-export class AuthMiddleware implements NestMiddleware {
-  private readonly logger = new Logger(AuthMiddleware.name);
-
-  // constructor(private readonly requestService: RequestService) {}
-
-  use(req: Request, res: Response, next: NextFunction) {
-    this.logger.log(AuthMiddleware.name);
-
-    const authToken = req.headers['authorization'];
-
-    if (!authToken) {
-      return res
-        .status(401)
-        .json({ message: 'Unauthorized - Missing Authorization Header' });
-    }
-
-    next();
-  }
-}
