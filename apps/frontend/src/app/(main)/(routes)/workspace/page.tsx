e7451b3664c424e06f9a501bcf8b5c4c40d4f436
ruralFluffyDill <stacylukavsky@gmail.com>
2023-12-09 23:08:53 -0500

    add new workspace component

---
+++ apps/frontend/src/app/(main)/(routes)/workspace/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Navigation from '../../_components/Navigation';
+
+export default function page() {
+  return <section className='h-[100dvh] top-[48px] bg-blue-200 flex flex-col items-center justify-center'>
+
+   <p>workspace</p>
+  </section>;
+}
