0b604f7f5681eb8bea159ce43a63f61f01634615
ruralFluffyDill <stacylukavsky@gmail.com>
2023-12-10 22:44:03 -0500

    add authProvider wrapper parent

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -1,11 +1,14 @@
 /* eslint-disable @next/next/no-sync-scripts */
+
 /* eslint-disable react/jsx-no-comment-textnodes */
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import React from 'react';
+import React, { useContext } from 'react';
 import Navbar from './(marketing)/_components/Navbar';
 import ThemeProvider from './_providers/ThemeProvider';
+import { AuthContext, AuthContextProvider } from './_store/authContext';
+import Providers from './_store/providers';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,22 +22,26 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  // const authContext = useContext(AuthContext);
+
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang='en' suppressHydrationWarning>
       <head></head>
       <body className={`${inter.className}`}>
-        <Navbar />
-        <main>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="light"
-            enableSystem
-            disableTransitionOnChange
-            storageKey="task-tide"
-          >
-            {children}
-          </ThemeProvider>
-        </main>
+        <AuthContextProvider>
+          <Navbar />
+          <main>
+            <ThemeProvider
+              attribute='class'
+              defaultTheme='light'
+              enableSystem
+              disableTransitionOnChange
+              storageKey='task-tide'
+            >
+              {children}
+            </ThemeProvider>
+          </main>
+        </AuthContextProvider>
       </body>
     </html>
   );
