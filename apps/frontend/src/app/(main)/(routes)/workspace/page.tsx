2f06959ddef8b766083dbc9427443ad23cdf6c7f
ruralFluffyDill <stacylukavsky@gmail.com>
2023-12-10 22:44:36 -0500

    add new workspacce skeleton

--- apps/frontend/src/app/(main)/(routes)/workspace/page.tsx
+++ apps/frontend/src/app/(main)/(routes)/workspace/page.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+'use client'
+import React, { useContext } from 'react';
 import Navigation from '../../_components/Navigation';
+import { AuthContext } from '@/app/_store/authContext';
 
-export default function page() {
+export default function Workspace() {
+  const aContext = useContext(AuthContext);
+
+  console.log({aContext})
   return <section className='h-[100dvh] top-[48px] bg-blue-200 flex flex-col items-center justify-center'>
 
    <p>workspace</p>
