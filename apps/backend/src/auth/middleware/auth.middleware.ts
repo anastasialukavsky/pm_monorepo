8418061fcf7bd32e13caeb563e1b09322040d332
ruralFluffyDill <stacylukavsky@gmail.com>
2023-12-16 21:56:11 -0500

    rewrote auth to read acc_token from the req.cookies.acc_token

--- apps/backend/src/auth/middleware/auth.middleware.ts
+++ apps/backend/src/auth/middleware/auth.middleware.ts
@@ -1,4 +1,9 @@
-import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import {
+  Injectable,
+  Logger,
+  NestMiddleware,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
 import { AuthService } from '../auth.service';
 import { JwtService } from '@nestjs/jwt';
@@ -57,17 +62,39 @@ import { Tokens } from '../types/index';
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
-    const authToken = req.headers['authorization'];
-    console.log('hello from middleware');
-    // if (!authToken) {
-    //   return res
-    //     .status(401)
-    //     .json({ message: 'Unauthorized - Missing Authorization Header' });
-    // } else {
-    console.log('REQUSR', req.user);
-    return req.user;
+  constructor(private authService: AuthService) {}
+  async use(req: Request, res: Response, next: NextFunction) {
+    // const authToken = req.headers['authorization'];
+    // console.log('hello from middleware');
 
+    // console.log('req headers', req.headers);
+    // console.log('REQ Cookies from middleware: ', req.cookies);
+    // if (!authToken)
+    //   throw new UnauthorizedException('Unauthorized - Invalid Token');
+
+    // const userId = await this.authService.checkUserAuth(
+    //   req.headers as Record<string, string>,
+    // );
+
+    // if (!userId)
+    //   throw new UnauthorizedException('Unauthorized - Invalid Token Sub');
+
+    // req.user = userId;
+
+    const access_token = req.cookies.access_token;
+
+    console.log('hilo form middleware');
+
+    if (!access_token)
+      throw new UnauthorizedException('Unauthorized - Invalid Access Token');
+
+    const userId = req.cookies.userId;
+    console.log({ 'userId from middleware': userId });
+
+    if (!userId)
+      throw new UnauthorizedException('Unauthorized- Invalid Token Sub');
+
+    req.user = userId;
     next();
   }
 }
