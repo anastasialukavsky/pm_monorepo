ca6405582e6d3172b82835c46b700caddb490807
ruralFluffyDill <stacylukavsky@gmail.com>
2024-01-16 20:57:49 -0500

    add user obj, add is loggedIn flag

--- apps/frontend/src/app/_store/authContext.tsx
+++ apps/frontend/src/app/_store/authContext.tsx
@@ -2,91 +2,53 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import Cookies from 'js-cookie';
+import { useRouter } from 'next/navigation';
+// import id from 'date-fns/esm/locale/id/index';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 const HTTP_ENDPOINT = process.env.NEXT_PUBLIC_API_ENDPOINT;
 export interface IGlobalAuthProps {
-  user: any;
+  user: {
+    userId: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+  };
   isLoading: boolean;
+  isLoggedIn: boolean;
+  setIsLoggedIn: (isLoggedIn: boolean) => void;
   setUser: (user: any) => void;
   setIsLoading: (loading: boolean) => void;
 }
 
 export const AuthContext = React.createContext<IGlobalAuthProps>({
-  user: {},
+  user: {
+    userId: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+  },
   isLoading: true,
+  isLoggedIn: false,
+  setIsLoggedIn: () => {},
   setUser: () => {},
   setIsLoading: () => {},
 });
 
-export const AuthContextProvider = (props: any) => {
-  const [user, setUser] = useState({});
+export const AuthContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [user, setUser] = useState({
+    userId: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+  });
   const [isLoading, setIsLoading] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  // const getCookie = (userId: string) => {
-  //   const cookie = Cookies.get(userId);
-  //   console.log({ cookie });
-  //   return cookie ? decodeURIComponent(cookie) : null;
-
-  // };
-
-  const getTokens = async () => {
-    try {
-      const userId = getCookie('user_id');
-
-      console.log({ 'login frontend userID': userId });
-      // if (!userId) {
-      //   console.error(`UserId ${userId} is not found`);
-      //   return;
-      // }
-
-      const response = await axios.get(
-        `${HTTP_ENDPOINT}/auth/get-tokens/${userId}`,
-        { withCredentials: true }
-      );
-
-      console.log({ response });
-      const { access_token, refresh_token } = response.data;
-
-      const headers = {
-        Authorization: `Bearer ${access_token}`,
-      };
-
-      return headers;
-    } catch (err) {
-      console.error('Error getting tokens: ', err);
-      throw err;
-    }
-  };
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const headers = await getTokens();
-        console.log({ headers });
-        const payload = await axios.get(
-          'http://localhost:3333/auth/authentication-check',
-          {
-            withCredentials: true,
-            headers: {
-              'Content-Type': 'application/json',
-              ...headers,
-            },
-          }
-        );
-        // console.log({ payload });
-        setUser(payload.data);
-        setIsLoading(false);
-        console.log('payload from context', payload.data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-        setIsLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  console.log({ user, isLoading });
   return (
     <AuthContext.Provider
       value={{
@@ -94,12 +56,11 @@ export const AuthContextProvider = (props: any) => {
         isLoading,
         setUser,
         setIsLoading,
+        isLoggedIn,
+        setIsLoggedIn,
       }}
     >
-      {props?.children}
+      {children}
     </AuthContext.Provider>
   );
 };
-function getCookie(arg0: string) {
-  throw new Error('Function not implemented.');
-}
