bffcbb22949be2db0644000183eb0a49ae13955f
ruralFluffyDill <stacylukavsky@gmail.com>
2024-02-03 22:32:30 -0500

    refactor

--- apps/frontend/src/app/(main)/(routes)/workspace/page.tsx
+++ apps/frontend/src/app/(main)/(routes)/workspace/page.tsx
@@ -6,7 +6,9 @@ import { AuthContext } from '@/app/_store/authContext';
 export default function Workspace() {
   const authContext = useContext(AuthContext);
 
+
   console.log({authContext})
+  // console.log(authContext.user.firstName)
   return <section className='h-[100dvh] top-[48px] bg-blue-200 flex flex-col items-center justify-center'>
 
    <p>workspace</p>
