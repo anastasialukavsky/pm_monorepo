67ad8524ace3e210419c19aac5e98a522abc86f2
ruralFluffyDill <stacylukavsky@gmail.com>
2024-02-03 22:35:15 -0500

    add usrer slice

---
+++ apps/frontend/src/app/_redux/slices/userSlice.ts
@@ -0,0 +1,21 @@
+// 'use client'
+
+// import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+// import axios, { AxiosError } from 'axios';
+// import { TReduxError, TReduxUser } from '../reduxTypes';
+// const HTTP_ENDPOINT = process.env.NEXT_PUBLIC_API_ENDPOINT;
+
+// export type UserState = {
+//   user: TReduxUser;
+//   loading: boolean;
+//   error: TReduxError;
+// };
+
+// const initialState: UserState = {
+//   user: {} as TReduxUser,
+//   loading: false,
+//   error: {
+//     status: null,
+//     message: null,
+//   },
+// };
